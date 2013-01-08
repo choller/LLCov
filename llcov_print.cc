956118de3748112b45e9f7f092965d54f6a0c352
Christian Holler (:decoder) <choller@mozilla.com>
2013-01-08 11:12:27 -0800

    Split llcov_print.cc

--- llcov_print.cc
+++ llcov_print.cc
@@ -1,9 +1,1 @@
-#include <stdio.h>
-#include <stdint.h>
-
-extern "C" void llvm_llcov_block_call(const char* funcname, const char* filename, uint32_t line) 
-	__attribute__((visibility("default")));
-
-extern "C" void llvm_llcov_block_call(const char* funcname, const char* filename, uint32_t line) {
-	fprintf(stderr, "Block executed in file %s, line %u (function %s)\n", filename, line, funcname);
-}
+llcov_assert.cc
